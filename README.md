a48528a506b7f786404341bd30c1b28dc48c8a20
Your Name <syu.m.5151@gmail.com>
2017-05-22 09:50:56 +0000

    day fin

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 ```
 
 ## csv format
-[[vlan numder or hostname],[0:permit or 1:deny],src-ip,wild card,src-port,dst-ip,wild card,dst-port]
+[[vlan numder or hostname],[0:permit or 1:deny],protocol,src-ip,wild card,src-port,dst-ip,wild card,dst-port]
 ## csv example
 1,0,10.0.0.0,24,0,192.168.0.0,24,80   
 1,0,10.0.0.0,24,0,192.168.0.0,24,443  
--- csv_acl.py
+++ csv_acl.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 #%!/bin/bash python3
 import sys
+import csv
+
+def add_acl():
+    pass
+
+def add_permit(permit):
+    if permit == 0:
+        return permit
+    if permit == 1:
+        return deny
+
+def main():
+    acl = "access-list"
+    csv_file_name = sys.argv[1]
+    acl_file_name = csv_file_name[:-4]+"_acl.txt"
+    print(acl_file_name)
+    with open(csv_file_name,'r') as f:
+        reader = csv.reader(f)
+
+        for row in reader:
+            for c in range(1,8):
+                print(row[c])
+
+main()
