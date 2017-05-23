8bb694fc228d36e5a4a709882b69a37ac9d69a10
Shuya Motouchi <syu.m.5151@gmail.com>
2017-05-23 10:31:39 +0900

    Update models.py

--- acl_admin/cms/models.py
+++ acl_admin/cms/models.py
@@ -3,14 +3,9 @@ from django.db import models
 # Create your models here.
 class ACL(models.Model):
     """Access_Control_List"""
-    vlan = models.IntegerField('vlan番号', blank=True, default=0)
-    protocol = models.CharField('プロトコル', max_length=15)
-    src_ip = models.CharField('送信元IP', max_length=64)
-    src_wc = models.CharField('送信元ワイルドカード', max_length=64)
-    src_port = models.CharField('送信元ポート番号', max_length=64)    
-    dst_ip = models.CharField('宛先IP', max_length=64)
-    dst_wc = models.CharField('宛先ワイルドカード', max_length=64)
-    dst_port = models.CharField('宛先ポート番号', max_length=64)
+    acl_id = models.IntegerField('acl_id', blank=True, defa)
+    acl = models.CharField('アクセスリストコントロール', max_length=225)
+    active = models.CharField('有効無効', )
 
     def __int__(self):
-            return self.vlan
+            return self.acl_id
