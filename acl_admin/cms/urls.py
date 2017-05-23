a357a2e02ac4ac620cf916919b786bb2da95c77f
Shuya Motouchi <syu.m.5151@gmail.com>
2017-05-23 09:43:51 +0900

    Update urls.py

--- acl_admin/cms/urls.py
+++ acl_admin/cms/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # ACL
     url(r'^acl/$', views.acl_list, name='acl_list'),   # 一覧
     url(r'^acl/add/$', views.acl_edit, name='acl_add'),  # 登録
-    url(r'^acl/mod/(?P<book_id>\d+)/$', views.acl_edit, name='acl_mod'),  # 修正
-    url(r'^acl/del/(?P<book_id>\d+)/$', views.acl_del, name='acl_del'),   # 削除
+    url(r'^acl/mod/(?P<vlan>\d+)/$', views.acl_edit, name='acl_mod'),  # 修正
+    url(r'^acl/del/(?P<vlan>\d+)/$', views.acl_del, name='acl_del'),   # 削除
         ]
